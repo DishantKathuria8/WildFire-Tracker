d636ae53748cbf791f895a66aeff2e1d31880a68
DishantKathuria8 <85367300+DishantKathuria8@users.noreply.github.com>
2022-01-27 02:25:20 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import {Icon} from '@iconify/react';
+import locationIcon from '@iconify/icons-mdi/fire-alert'
+
+const Header = () => {
+  return <header className='header'>
+      <h1><Icon icon={locationIcon}/>WildLife Tracker (Powered By NASA)</h1>
+  </header>;
+};
+
+export default Header;
---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+
+const Loader = () => {
+  return (
+  <div className='loader'>
+      <img src="https://i.stack.imgur.com/ATB3o.gif" alt="Loading" />
+      <h1>Fetching Data</h1>
+  </div>
+  );
+};
+
+export default Loader;
+
